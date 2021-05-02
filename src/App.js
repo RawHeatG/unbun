4fba43d1985f6b79abed2855efa342f0d424d9ad
Rohit Guati <rawheat.gulati@hotmail.com>
2021-05-02 07:14:32 +0530

    Initialize with CRA

---
+++ src/App.js
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="nav">
+        <h1>Unbun</h1>
+        <div class="nav-right">
+          <ul class="nav-menu">
+            <li>
+              <a href="index.html">Link1</a>
+            </li>
+            <li>
+              <a>Link2</a>
+            </li>
+            <li>
+              <a>Link3</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
